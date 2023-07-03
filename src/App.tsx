48ba3d47ab91710e6457a9fe39473aeba20fc319
Manudev <manudev.rethina@solitontech.com>
2023-07-03 11:51:09 +0530

    lkjh

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Budget } from './components/budget'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/BudgetTrack">
       <Routes>
         <Route path="/" element={<Budget />} />
         <Route path="*" element={<Budget />} />
