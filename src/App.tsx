a217b743ecb57953dfa83b705f73f8c51f0abe1f
Manudev <manudev.rethina@solitontech.com>
2023-07-03 10:37:20 +0530

    second commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
 import { Budget } from './components/budget'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <>
-    <Budget/>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Budget />} />
+        <Route path="*" element={<Budget />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
